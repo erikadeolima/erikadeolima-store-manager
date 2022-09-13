86b9477eec5952953f9d28cdd4d316c162f35acf
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-09-13 15:11:39 -0300

    fix:fix func consult in  productsModel

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -6,12 +6,6 @@ const consult = async () => {
   return product;
 };
 
-const consultProductId = async () => {
-  const query = 'SELECT * FROM StoreManager.products';
-  const [product] = await connection.execute(query);
-  return product;
-};
-
 const consultById = async (id) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id= ?';
   const [product] = await connection.execute(query, [id]);
